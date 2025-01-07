4de8d35e99cdfa6c3c38d9404e6ad6a6fe932032
@u632469 <cnalegrebustos@teco.com.ar>
2025-01-07 05:11:11 -0300

    cambios en el middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,6 @@ import { NextResponse } from 'next/server';
 
 export function middleware(req: { cookies: { get: (arg0: string) => any; }; url: string | URL | undefined; }) {
   const token = req.cookies.get('session_token'); // Obtener el token de sesión de las cookies
-  console.log(token)
   if (!token) {
     return NextResponse.redirect(new URL('/crm/login', req.url)); // Redirigir al login si no hay sesión
   }
@@ -11,5 +10,5 @@ export function middleware(req: { cookies: { get: (arg0: string) => any; }; url:
 }
 
 export const config = {
-  matcher: ['/crm/:path*'], // Protege todas las rutas bajo /crm
+  matcher: ['/crm/dashboard/:path*'], // Protege todas las rutas bajo /crm
 };
